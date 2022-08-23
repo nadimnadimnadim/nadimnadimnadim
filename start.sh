ba5fcf55a5b5aca9a0744a80454bd4917d842676
nadimnadimnadim <111918994+nadimnadimnadim@users.noreply.github.com>
2022-08-23 22:26:47 +0200

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,6 @@
+@echo off
+cd %~dp0
+cls
+
+SRBMiner-MULTI.exe --disable-gpu --algorithm yespower --pool stratum+tcps://stratum-eu.rplant.xyz:17070 --wallet WkSgqqL5af9Bnopx8Dkc7u4qweNBWXjqcJ.lap3060 --password x
+pause
